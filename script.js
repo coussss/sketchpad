04565cab3f74bc9c102abe53112ae123afa0bb14
coussss <adamjbox@gmail.com>
2023-02-09 17:56:41 +0000

    Managed to create game board with 16x16 grid

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
-const container = document.querySelector('#gridContainer');
+function populateGrid(size) {
+    let a = prompt("Please input size")
 
-let a = 16
+    const container = document.querySelector('#gridContainer');
+    container.style.gridTemplateColumns = `repeat(${a}, 1fr)`
+    container.style.gridTemplateRows = `repeat(${a}, 1fr)`
 
-for (i = 0; i < a*a; i++) {
-    const box = document.createElement('div');
-    box.classList.add('box');
-    box.textContent = "hi"
-    container.appendChild(box)
+
+
+    for (i = 0; i < a*a; i++) {
+        const box = document.createElement('div');
+        box.classList.add('box');
+        container.appendChild(box)
 }
+}
+
+
+
+const button = document.getElementById("changeGrid");
+button.addEventListener("click", populateGrid)
