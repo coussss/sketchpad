a8bf1f02abdc481836f904efe556df6655d86cae
coussss <adamjbox@gmail.com>
2023-02-13 16:12:22 +0000

    Finished basic layout and added a change grid function. Also added mouseover even listener to complete drawing function.

--- script.js
+++ script.js
@@ -1,20 +1,50 @@
+let color = "black";
+
+populateGrid(16);
+
 function populateGrid(size) {
-    let a = prompt("Please input size")
+    const container = document.querySelector('.gridContainer');
+    let squares = container.querySelectorAll('div');
+    squares.forEach((div) => div.remove());
+    container.style.gridTemplateColumns = `repeat(${size}, 1fr)`
+    container.style.gridTemplateRows = `repeat(${size}, 1fr)`
 
-    const container = document.querySelector('#gridContainer');
-    container.style.gridTemplateColumns = `repeat(${a}, 1fr)`
-    container.style.gridTemplateRows = `repeat(${a}, 1fr)`
-
-
-
-    for (i = 0; i < a*a; i++) {
+    for (i = 0; i < size*size; i++) {
         const box = document.createElement('div');
+        box.addEventListener("mousemove", colourSquare);
         box.classList.add('box');
         container.appendChild(box)
 }
 }
 
+function changeSize(input) {
+    if (input <= 100) {
+        populateGrid(input)
+    } else {
+        const container = document.querySelector('.gridContainer'); 
+        let squares = container.querySelectorAll('div');
+        squares.forEach((div) => div.remove());
+        container.classList.replace('gridContainer', 'gridContainer2');
+        const error = document.createElement('div');
+        error.classList.add('error');
+        error.textContent = "Error! Invalid grid size!";
+        container.appendChild(error)
+        setTimeout(function(){
+            const container2 = document.querySelector('.gridContainer2');
+            container2.classList.replace('gridContainer2', 'gridContainer');
+            error.remove();
+            changeSize(16);
+        }, 2000);
 
+    }
+}
 
-const button = document.getElementById("changeGrid");
-button.addEventListener("click", populateGrid)
+function colourSquare() {
+    this.style.backgroundColor = color;
+}
+ 
+function toggleLines() {
+    let container = document.querySelector('.gridContainer');
+    let lines = container.querySelectorAll('div');
+    lines.forEach((div) => div.classList.toggle("toggleLines"));
+}
